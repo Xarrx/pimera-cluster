e4afc5c3193492a909f681c3a1dcc28f9c715a2c
Xarrx <damian.h.kendzior@gmail.com>
2020-05-29 14:05:56 -0500

    Adjusted node color value.

--- fitStatUSB-scripts/fitStatUSB-setup.sh
+++ fitStatUSB-scripts/fitStatUSB-setup.sh
@@ -1,4 +1,4 @@
 #/bin/sh -e
 stty -F /dev/ttyACM0 9600 raw -echo -echoe -echok -echoctl -echoke
-echo "B#000010-0100#000010-0100" > /dev/ttyACM0
+echo "B#000010-0100#000020-0100" > /dev/ttyACM0
 echo "F1000" > /dev/ttyACM0
