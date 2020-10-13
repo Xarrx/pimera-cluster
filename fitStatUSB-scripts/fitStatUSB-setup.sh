b4c9c0e73246008b6d268c56d883be6ab8cfc534
Xarrx <damian.h.kendzior@gmail.com>
2020-10-13 12:09:52 -0500

    Changed client default LED color to blue. Changed server default LED color to green.

--- fitStatUSB-scripts/fitStatUSB-setup.sh
+++ fitStatUSB-scripts/fitStatUSB-setup.sh
@@ -1,4 +1,4 @@
 #/bin/sh -e
 stty -F /dev/ttyACM0 9600 raw -echo -echoe -echok -echoctl -echoke
-echo "B#000010-0100#000020-0100" > /dev/ttyACM0
+echo "B#002000-0200#004000-0200" > /dev/ttyACM0
 echo "F1000" > /dev/ttyACM0
