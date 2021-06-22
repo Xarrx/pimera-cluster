5f496677004d9af7577d4e94282e52744e4419bd
Xarrx <damian.h.kendzior@gmail.com>
2021-06-22 13:13:58 -0500

    Removed unused directories. Removed prototype scripts that were no longer needed (all of them).

--- prototype-scripts/get-eth0-mac.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#IFS=' ' read -r -a a <<< $(ifconfig eth0 | grep ether) && echo ${a[1]}
-#str=$(ifconfig eth0 | grep ether) && echo $str | cut -d ' ' -f 2
-#str=$(ip address show dev eth0 | grep ether) && echo $str | cut -d ' ' -f 2
-# get macs for all nodes in cluster
-pdsh -a "ip address show dev eth0 | awk '/ether/ {print \$2}'" | sort
